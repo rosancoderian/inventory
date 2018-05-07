9f95f71648b229df71b119b437e3e7eee2ebc501
rosancoderian <rosan@localhost.localdomain>
2018-05-07 15:40:59 +0700

    add create-modal in item-table

--- components/item-table.js
+++ components/item-table.js
@@ -1,3 +1,7 @@
+import { Component } from 'react'
+import Rodal from 'rodal'
+import 'rodal/lib/rodal.css'
+
 const NoDataRow = () => (
 <tr>
     <td colSpan="5" className="text-center">
@@ -28,36 +32,70 @@ const ItemRow = (props) => (
 </tr>
 )
 
-export const ItemTable = ({ data = [] }) => (
-<div className="card">
-    <div className="card-header">
-        <h3 className="card-title">Items</h3>
-        <div className="card-options">
-            <a href="#" className="btn btn-primary btn-sm"><i className="fe fe-plus"></i></a>
-        </div>
-    </div>
-    <div className="table-responsive">
-        <div className="table table-hover table-outline table-vcenter text-nowrap card-table">
-            <table className="table table-hover table-outline table-vcenter text-nowrap card-table">
-                <thead>
-                    <tr>
-                        <th className="text-center w-1">#</th>
-                        <th>Name</th>
-                        <th className="text-right">Stock</th>
-                        <th className="text-left">Desc</th>
-                        <th className="w-1"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.length ? data.map((d, i) => <ItemRow key={i} i={i} {...d} />) : <NoDataRow />}
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <style jsx>{`
-    table {
-        background-color: #fff !important;
-    }
-    `}</style>
-</div>
+const CreateModal = (props) => (
+    <Rodal {...props}>
+        <div>Content</div>
+    </Rodal>
 )
+
+export class ItemTable extends Component {
+    constructor () {
+        super()
+        this.state = {
+            createModalVisible: false
+        }
+    }
+
+    render () {
+        let { data } = this.props
+        let { createModalVisible } = this.state
+        return (
+        <div className="card">
+            <CreateModal visible={createModalVisible} onClose={this.hideCreateModal.bind(this)} />
+            <div className="card-header">
+                <h3 className="card-title">Items</h3>
+                <div className="card-options">
+                    <a href="#" className="btn btn-primary btn-sm" onClick={this.showCreateModal.bind(this)}><i className="fe fe-plus"></i></a>
+                </div>
+            </div>
+            <div className="table-responsive">
+                <div className="table table-hover table-outline table-vcenter text-nowrap card-table">
+                    <table className="table table-hover table-outline table-vcenter text-nowrap card-table">
+                        <thead>
+                            <tr>
+                                <th className="text-center w-1">#</th>
+                                <th>Name</th>
+                                <th className="text-right">Stock</th>
+                                <th className="text-left">Desc</th>
+                                <th className="w-1"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.length ? data.map((d, i) => <ItemRow key={i} i={i} {...d} />) : <NoDataRow />}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <style jsx>{`
+            table {
+                background-color: #fff !important;
+            }
+            `}</style>
+        </div>
+        )
+    }
+
+    showCreateModal () {
+        this.setState({
+            ...this.state,
+            createModalVisible: true
+        })
+    }
+
+    hideCreateModal () {
+        this.setState({
+            ...this.state,
+            createModalVisible: false
+        })
+    }
+}
