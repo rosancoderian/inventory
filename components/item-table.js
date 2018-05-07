1bda1c33b127b858a5778eaf364fa5521aa5d722
rosancoderian <rosan@localhost.localdomain>
2018-05-07 18:06:15 +0700

    add item form, delete item

--- components/item-table.js
+++ components/item-table.js
@@ -5,38 +5,100 @@ import 'rodal/lib/rodal.css'
 const NoDataRow = () => (
 <tr>
     <td colSpan="5" className="text-center">
-        Data not found.
+        Loading data.
     </td>
 </tr>
 )
 
-const ItemRow = (props) => (
-<tr>
-    <td className="text-center w-1">{props.i + 1}</td>
-    <td>{props.name}</td>
-    <td className="text-right">{props.stock}</td>
-    <td className="text-left">{props.desc}</td>
-    <td className="col-action">
-        <a href="#" className="icon">
-            <i className="fe fe-edit"></i>
-        </a>
-        <a href="#" className="icon">
-            <i className="fe fe-trash"></i>
-        </a>
-    </td>
-    <style jsx>{`
-    .col-action a {
-        margin: 0px 5px;
+class ItemRow extends Component {
+    constructor() {
+        super()
     }
-    `}</style>
-</tr>
-)
 
-const CreateModal = (props) => (
-    <Rodal {...props}>
-        <div>Content</div>
-    </Rodal>
-)
+    render () {
+        let props = this.props
+        return (
+        <tr data-id={props.id}>
+            <td className="text-center w-1">{props.i + 1}</td>
+            <td>{props.name}</td>
+            <td className="text-right">{props.stock}</td>
+            <td className="text-left">{props.desc}</td>
+            <td className="col-action">
+                <a className="icon">
+                    <i className="fe fe-edit"></i>
+                </a>
+                <a className="icon" onClick={this.delete.bind(this)}>
+                    <i className="fe fe-trash"></i>
+                </a>
+            </td>
+            <style jsx>{`
+            .col-action a {
+                margin: 0px 5px;
+            }
+            `}</style>
+        </tr>
+        )
+    }
+
+    async delete () {
+        await this.props.onDelete(this.props.id)
+        return true
+    }
+}
+
+export class ItemForm extends Component {
+    constructor () {
+        super()
+        this.state = {
+            name: '',
+            desc: ''
+        }
+    }
+
+    render () {
+        let { title = '', visible = true, onSave = () => {} } = this.props
+        let { name, desc } = this.state
+        let style = !visible ? { display: 'none' } : {}
+        return (
+        <div className="card" style={style}>
+            <div className="card-header">
+                <h3 className="card-title">{title}</h3>
+            </div>
+            <div className="card-body">
+                <div className="form-group">
+                    <label className="form-label">Name</label>
+                    <input type="text" className="form-control" placeholder="Name" value={name} onChange={this.createOnChangeListener('name')} />
+                </div>
+                <div className="form-group">
+                    <label className="form-label">Desc</label>
+                    <textarea type="text" className="form-control" placeholder="Desc" value={desc} onChange={this.createOnChangeListener('desc')} ></textarea>
+                </div>
+            </div>
+            <div className="card-footer">
+                <a href="#" className="btn btn-primary btn-sm float-right" onClick={this.save.bind(this)}><i className="fe fe-save"></i> Save</a>
+            </div>
+        </div>
+        )
+    }
+
+    async save () {
+        await this.props.onSave(this.state)
+        this.setState({
+            ...this.state,
+            name: '',
+            desc: ''
+        })
+        return true
+    }
+
+    createOnChangeListener (key) {
+        return (ev) => {
+            this.setState({
+                [key]: ev.target.value
+            })
+        }
+    }
+}
 
 export class ItemTable extends Component {
     constructor () {
@@ -47,11 +109,10 @@ export class ItemTable extends Component {
     }
 
     render () {
-        let { data } = this.props
+        let { data, onDelete = () => {} } = this.props
         let { createModalVisible } = this.state
         return (
         <div className="card">
-            <CreateModal visible={createModalVisible} onClose={this.hideCreateModal.bind(this)} />
             <div className="card-header">
                 <h3 className="card-title">Items</h3>
                 <div className="card-options">
@@ -71,7 +132,7 @@ export class ItemTable extends Component {
                             </tr>
                         </thead>
                         <tbody>
-                            {data.length ? data.map((d, i) => <ItemRow key={i} i={i} {...d} />) : <NoDataRow />}
+                            {data.length ? data.map((d, i) => <ItemRow key={i} i={i} onDelete={onDelete} {...d} />) : <NoDataRow />}
                         </tbody>
                     </table>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { db } from '../lib/db'
 import { Component } from 'react' 
 import { Page } from '../components/page'
-import { ItemTable } from '../components/item-table'
+import { ItemTable, ItemForm } from '../components/item-table'
 
 export default class IndexPage extends Component {
     static async getInitialProps (ctx) {
@@ -12,6 +12,7 @@ export default class IndexPage extends Component {
         super()
         this.state = {
             items: [],
+            addItemFormVisible: true
         }
     }
 
@@ -20,21 +21,44 @@ export default class IndexPage extends Component {
     }
 
     render () {
-        let { items } = this.state
+        let { items, addItemFormVisible } = this.state
         return (
         <Page>
-            <div className="row row-cards row-deck">
-                <div className="col-12">
-                    <ItemTable data={items} />
+            <div className="row">
+                <div className="col-8">
+                    <div className="row">
+                        <div className="col-12">
+                            <ItemTable data={items} onEdit={''} onDelete={this.deleteItem.bind(this)} />
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="row">
+                        <div className="col-12">
+                            <ItemForm title="Add Item" visible={addItemFormVisible} onSave={this.addItem.bind(this)} />
+                            <ItemForm title="Update Item" visible={!addItemFormVisible} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </Page>
         )
     }
 
+    async addItem ({ name, desc }) {
+        return db.collection('items').add({
+            name,
+            desc
+        })
+    }
+
+    async deleteItem (id) {
+        return db.collection('items').doc(id).delete()
+    }
+
     initDbListener () {
         db.collection('items').onSnapshot((snapshot) => {
-            let items = snapshot.docs.map(doc => doc.data())
+            let items = snapshot.docs.map(doc => ({id: doc.id, ...doc.data()}))
             this.setState({
                 ...this.state,
                 items
